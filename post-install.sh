0afed9e881b04eabbd62f66bc203b700c6f7ba50
Hassan Amouhzi <github@anezi.net>
2013-08-19 14:24:01 +0200

    Create post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir cache
+
+mkdir cache/comments
+
+mkdir cache/userfeeds
+
+mkdir files/conversion_queue
+
+mkdir files/logs
+
+mkdir files/mass_uploads
+
+mkdir files/original
+
+mkdir images/backgrounds
+
